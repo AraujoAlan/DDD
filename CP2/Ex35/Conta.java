19d66f42c20651896aa3a4837b3c8f92af83d370
Alan Araujo <80415927+AraujoAlan@users.noreply.github.com>
2022-05-04 17:20:04 -0300

    Add files via upload

---
+++ CP2/Ex35/Conta.java
@@ -0,0 +1,17 @@
+package Ex35;
+
+public class Conta {
+    public String agencia;
+    public String numero;
+    public double saldo;
+
+    Conta(String agencia, String numero, double saldo){
+        this.agencia = agencia;
+        this.numero = numero;
+        this.saldo = saldo;
+    }
+
+    Conta(){
+
+    }
+}
